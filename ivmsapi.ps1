dc4f3c41e3168daaf6c41622be01964e9f6a5215
Sajan Shakya <sdshakya@Sajans-MacBook-Pro.local>
2024-05-17 16:54:40 +0545

    DockerSession: Uncommented code block from the script

--- ivmsapi.ps1
+++ ivmsapi.ps1
@@ -12,19 +12,19 @@ New-PolarisRoute -Path "/api/AverageAttDiff" -Method GET -Scriptblock {
     $Response.Json(($data | select personName, avgCheckInDiff | ConvertTo-Json))
 }
 
-# New-PolarisRoute -Path "/api/person/:id" -Method GET -Scriptblock {
-#     $personName = $Request.Parameters.id
-#     $personName = $personName.Replace('%20',' ')
-#     $query = "exec USP_GetUserAverageAttDiff '" + $personName + "'"
-#     $data = Invoke-Sqlcmd -ServerInstance $sqlHost -Database $sqlDatabase -Username $sqlUserName -Password $sqlPassword -Query $query -TrustServerCertificate
-#     if($data.Count -eq 0) {
-#         $Response.SetStatusCode(404)
-#         $Response.Send("Person $($Request.Parameters.id) not found!")
-#     }
-#     else {
-#         $Response.Json(($data | select personName, avgCheckInDiff | ConvertTo-Json))
-#     }    
-# }
+ New-PolarisRoute -Path "/api/person/:id" -Method GET -Scriptblock {
+     $personName = $Request.Parameters.id
+     $personName = $personName.Replace('%20',' ')
+     $query = "exec USP_GetUserAverageAttDiff '" + $personName + "'"
+     $data = Invoke-Sqlcmd -ServerInstance $sqlHost -Database $sqlDatabase -Username $sqlUserName -Password $sqlPassword -Query $query -TrustServerCertificate
+     if($data.Count -eq 0) {
+         $Response.SetStatusCode(404)
+         $Response.Send("Person $($Request.Parameters.id) not found!")
+     }
+     else {
+         $Response.Json(($data | select personName, avgCheckInDiff | ConvertTo-Json))
+     }    
+ }
 
 $app = Start-Polaris -Port 8082 -MinRunspaces 1 -MaxRunspaces 5 -UseJsonBodyParserMiddleware -Verbose # all params are optional
 
